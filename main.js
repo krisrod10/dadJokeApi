d96e65dbb66ff6009937d75c7df9100af37efaed
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2021-08-21 18:51:11 -0500

    update

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
-const button = document.querySelector(".container button");
+const button = document.querySelector(".container button"); // linking to my container id inside my html 
 const jokeDiv = document.querySelector(".container .joke p");
+const button2 = document.querySelector(".container button2");
 
-document.addEventListener("DOMContentLoaded", getJock);
+document.addEventListener("DOMLoaded", getJock); // add event listener to get the joke when called upon 
 
-button.addEventListener("click", getJock);
+button.addEventListener("click", getJock); //button clicker to get dad jokes
+button2.addEventListener("click",getJock);
+async function getJock() { //function use to generate jokes 
+    const jokeData = await fetch("https://icanhazdadjoke.com/", { //fetching the data from the dad joke api 
+        headers: {
+            Accept: "application/json"
+        }
+    });
+    const jokeObj = await jokeData.json();
+    jokeDiv.innerHTML = jokeObj.joke;
+    console.log(jokeData);
+}
+// when i was googling and found the async and the await function use i dont remeber being using it in class but i saw it was a good use for the extension of the promise \
 
-async function getJock() {
-  const jokeData = await fetch("https://icanhazdadjoke.com/", {
-    headers: {
-      Accept: "application/json"
-    }
-  });
-  const jokeObj = await jokeData.json();
-  jokeDiv.innerHTML = jokeObj.joke;
-  console.log(jokeData);
+async function getJoke2(){
+    const jokeDataTwo = await fetch("https://icanhazdadjoke.com/", {
+        headers:{
+            Accept: "application/json"
+        }
+    });
+    const jokeObjTwo = await jokeDataTwo.json();
+    jokeDiv.innerHTML = jokeObjTwo.joke;
+    console.log(jokeDataTwo)
 }
